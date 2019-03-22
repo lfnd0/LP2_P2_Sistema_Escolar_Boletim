f5b7c147851a10506e34ace058a91d97390392ee
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-03-22 19:36:17 -0300

    Inicialização do projeto

---
+++ VideoGameShop/src/main/java/br/edu/vgs/VideoGameShopApplication.java
@@ -0,0 +1,13 @@
+package br.edu.vgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameShopApplication.class, args);
+	}
+
+}
